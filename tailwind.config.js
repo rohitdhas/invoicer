c533da3f6300b8ae8ae62f3ea0b7605d7f1c1bef
Rohit Dhas <rohitdhas666@gmail.com>
2022-06-04 19:00:07 +0530

    updated color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      primary: {
+        100: "#FAF1F0",
+        200: "#FBAFA7",
+        300: "#FF6150",
+      },
+      secondary: {
+        100: "#EDE3FF",
+        200: "#BEA4EF",
+        300: "#723DD8",
+      },
+    },
   },
   plugins: [],
-}
+};
